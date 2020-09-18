cff83b40710e87edd54b9726604c5eda71f4fe56
manueldelapenna <manueldelapenna@gmail.com>
2020-09-18 17:07:27 -0300

    cambio path de ejemplo

--- example/example.js
+++ example/example.js
@@ -1,11 +1,11 @@
 "use strict";
-import {ServiceWorkerAdmin, Options} from "../dist/service-worker-admin";
+import {ServiceWorkerAdmin} from "../dist/service-worker-admin";
 window.onload=function(){
-    var options: Options={
+    var options={
         onEachFile: ()=>console.log('on each file'),
         onStartLoading: ()=>console.log('starting download'),
         onError: (err)=>console.log('on error', err),
-        onNewVersionAvailable: (version:string)=>console.log('on new version available ', version)
+        onNewVersionAvailable: (version)=>console.log('on new version available ', version)
     }
     var swa = new ServiceWorkerAdmin(options)
     //swa.installFrom('./example-for-cache.json')

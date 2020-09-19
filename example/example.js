e1f6aa8eed850a9482775866d257d6767baff64c
Emilio Platzer <emilioplatzer@gmail.com>
2020-09-18 22:10:47 -0300

    ejemplo con consola sobre la página y aplicación calculadora

--- example/example.js
+++ example/example.js
@@ -1,14 +1,41 @@
 "use strict";
 //import {ServiceWorkerAdmin} from "../dist/service-worker-admin";
 var ServiceWorkerAdmin=require("../dist/service-worker-admin.js").ServiceWorkerAdmin;
+
+function console_log(message, obj){
+    var div = document.createElement('div');
+    div.appendChild(document.createTextNode(message));
+    div.appendChild(document.createTextNode(JSON.stringify(obj)));
+    document.getElementById('console').appendChild(div);
+}
+
 window.onload=function(){
     var options={
-        onEachFile: ()=>console.log('on each file'),
-        onStartLoading: ()=>console.log('starting download'),
-        onError: (err)=>console.log('on error', err),
-        onNewVersionAvailable: (version)=>console.log('on new version available ', version)
+        onEachFile: (f)=>console_log('file: ',f),
+        onStartLoading: ()=>console_log('starting download'),
+        onError: (err)=>console_log('on error', err),
+        onNewVersionAvailable: (version)=>console_log('new version available: ', version)
     }
     var swa = new ServiceWorkerAdmin(options)
     //swa.installFrom('./example-for-cache.json')
     console.log("swa ", swa)
+    document.getElementById('calcular').addEventListener('click',function(){
+        var visor = document.getElementById('visor');
+        var calculo = visor.value;
+        var resultado;
+        var color;
+        try{
+            resultado = new Function('return '+calculo)();
+            color='blue';
+        }catch(err){
+            resultado = err.message;
+            color='red';
+        }
+        var div=document.createElement('div');
+        div.color=color;
+        div.appendChild(document.createTextNode(calculo))
+        div.appendChild(document.createTextNode(' ⇨ '))
+        div.appendChild(document.createTextNode(resultado))
+        document.getElementById('history').appendChild(div);
+    })
 }
